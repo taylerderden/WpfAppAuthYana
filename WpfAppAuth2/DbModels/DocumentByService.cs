c37a5fde90557e73a105aeb4c9108705c8181163
taylerderden <danizlobin_2004@mail.ru>
2023-12-07 14:05:52 +0400

    Добавьте файлы проекта.

---
+++ WpfAppAuth2/DbModels/DocumentByService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfAppAuth2.DbModels;
+
+public partial class DocumentByService
+{
+    public int Id { get; set; }
+
+    public int ClientServiceId { get; set; }
+
+    public string DocumentPath { get; set; } = null!;
+
+    public virtual ClientService ClientService { get; set; } = null!;
+}
